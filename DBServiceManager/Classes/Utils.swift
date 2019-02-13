82fce268355d9959353623766947047b75c0a1ee
Massimiliano <massimiliano@Wolf-pippo.local>
2019-02-13 11:45:05 +0100

    Added encodable extension

--- DBServiceManager/Classes/Utils.swift
+++ DBServiceManager/Classes/Utils.swift
@@ -5,8 +5,22 @@
 //  Created by Massimiliano on 13/02/2019.
 //
 
-import UIKit
-
-class Utils: NSObject {
-
+extension Encodable {
+    public func asDictionary() throws -> [String: Any] {
+        let data = try JSONEncoder().encode(self)
+        guard let dictionary = try JSONSerialization.jsonObject(with: data, options: .allowFragments) as? [String: Any] else {
+            throw NSError()
+        }
+        return dictionary
+    }
+    
+    var dictionary: [String: Any] {
+        get {
+            if let dict = try? self.asDictionary() {
+                return dict
+            } else {
+                return [:]
+            }
+        }
+    }
 }
